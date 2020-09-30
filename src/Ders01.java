dd5ca4a9dedea104ea8422f6f7dba48b78742a10
SelimAkaslan <67130358+SelimAkaslan@users.noreply.github.com>
2020-09-30 16:42:14 +0200

    Update Ders01.java

--- src/Ders01.java
+++ src/Ders01.java
@@ -4,6 +4,7 @@ public class Ders01 {
 	public static void main(String[] args) {
 		System.out.println("Saat 17:52");
 		System.out.println("Saat 18:00");
+		System.out.println("Saat 21:00 Toplanti basliyor");
 
 	}
 
